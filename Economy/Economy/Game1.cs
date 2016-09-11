e730bf3fdfa957a9af327bee6ff82245509f6dc6
Nicolas <Nicolas@NICOLAPTOP>
2016-09-11 17:00:23 +0200

    Nouveau système de map. Problème de personnage qui ne s'affiche pas en cours de résolution...

--- Economy/Economy/Game1.cs
+++ Economy/Economy/Game1.cs
@@ -16,6 +16,7 @@ using FarseerPhysics.Factories;
 using FarseerPhysics.Collision;
 using Personnage;
 using Mob;
+using Maps;
 
 
 
@@ -26,21 +27,17 @@ namespace Economy
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D map;
         Texture2D personnage;
-        Rectangle mainFrame;
+        //Rectangle mainFrame;
         KeyboardState oldState;
         int angle = 0;
         MouseState oldSourisState;
         Perso perso = new Perso();
         mob[] Mob = new mob[2];
-        Rectangle mapCollison;
+        Map[] maps = new Map[100];
 
    //     int sol = 250;
         float limiteSaut;
-
-        Color[] mapTextureData;
-        Color[] persoTextureData;
 
         public Game1()
         {
@@ -54,10 +51,14 @@ namespace Economy
             this.graphics.IsFullScreen = false;
             this.graphics.ApplyChanges();
             this.Window.Title = "Jeu";
-            mainFrame = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+           // mainFrame = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
             for (int i = 0; i <= 1; i++)
             {
                 Mob[i] = new mob();
+            }
+            for (int i = 0; i < maps.Length; i++)
+            {
+                maps[i] = new Map();
             }
             Mob[0].moveMob(new Vector2(500, 250));
             Mob[1].moveMob(new Vector2(600, 250));
@@ -67,16 +68,13 @@ namespace Economy
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            map = Content.Load<Texture2D>("map");
-            mapCollison = GetSmallestRectangleFromTexture(map);
-            mapTextureData =
-                new Color[map.Width * map.Height];
-            map.GetData(mapTextureData);
+            for (int i = 0; i < maps.Length; i++)
+            {
+                maps[i].texture = Content.Load<Texture2D>("plateforme");
+            }
             perso.perso = Content.Load<Texture2D>("perso");
             perso.perso2 = Content.Load<Texture2D>("perso2");
-            persoTextureData =
-                new Color[perso.perso.Width * perso.perso.Height];
-            perso.perso.GetData(persoTextureData);
+            personnage = perso.perso;        
             perso.attack = Content.Load<Texture2D>("attack");
             for (int i = 0; i <= 1; i++)
             {
@@ -102,7 +100,11 @@ namespace Economy
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            spriteBatch.Draw(map, mainFrame, Color.White);
+
+            spawn(personnage, perso.getPersoPos(), angle, 1f); //On affiche le personnage
+            spawn(maps[0].texture, new Vector2(150, 400), 0, 1f);  
+            maps[0].hititBox = new Rectangle((int)perso.getPersoPos().X, (int)perso.getPersoPos().Y, 64, 64);
+
 
             if (perso.getSens() == 1) //Si le perso est dans le sens 1
             {
@@ -110,47 +112,49 @@ namespace Economy
                 perso.createPersoHitBox(new Rectangle((int)perso.getPersoPos().X, (int)perso.getPersoPos().Y, 64, 64));
                 perso.moveAttack(new Vector2(perso.getPersoPos().X + 50, perso.getPersoPos().Y)); //et on positionne l'endroit o� appara�tra son attaque si il attaque
             }
-            else if (perso.getSens() == 0) //Si il est dans le sens 0
+            else  //Si il est dans le sens 0
             {
                 personnage = perso.perso2;
                 perso.createPersoHitBox(new Rectangle((int)perso.getPersoPos().X, (int)perso.getPersoPos().Y, 64, 64));
                 perso.moveAttack(new Vector2(perso.getPersoPos().X - 50, perso.getPersoPos().Y)); //idem
             }
 
-            spawn(personnage, perso.getPersoPos(), angle, 1f); //On affiche le personnage
 
-            if (IntersectPixels(mapCollison, mapTextureData, perso.getPersoHitBox(), persoTextureData) && perso.getPersoPos().Y > limiteSaut)
+            for (int i = 0; i < maps.Length; i++)
             {
-                // Si le personnage touche la map ET que sa position est plus basse que sa limite de saut c'est qu'il a touché le sol ALORS
-                // Il marche
-                // Il peut à nouveau sauter
-                perso.walking = true;
-                perso.canJump = true;
-            }
+                if (maps[i].hititBox.Intersects(perso.getPersoHitBox()) && perso.getPersoPos().Y > limiteSaut)
+                {
+                    // Si le personnage touche la map ET que sa position est plus basse que sa limite de saut c'est qu'il a touché le sol ALORS
+                    // Il marche
+                    // Il peut à nouveau sauter
+                    perso.walking = true;
+                    perso.canJump = true;
+                }
 
-            if (perso.isJumpingOrNot() == true)
-            {
-                // Si le perso est en train de sauter
-                // Il monte
-                perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y - 4));
-            }
+                if (perso.isJumpingOrNot() == true)
+                {
+                    // Si le perso est en train de sauter
+                    // Il monte
+                    perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y - 4));
+                }
 
-            if (!perso.walking && !perso.isJumpingOrNot() )
-            {
-                // Si le perso ne marche pas et ne saute pas non plus ALORS il tombe
-                perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y + 4));
-            }
+                if (!perso.walking && !perso.isJumpingOrNot())
+                {
+                    // Si le perso ne marche pas et ne saute pas non plus ALORS il tombe
+                    perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y + 4));
+                }
 
-            if (perso.getPersoPos().Y <= limiteSaut || IntersectPixels(mapCollison, mapTextureData, perso.getPersoHitBox(), persoTextureData))
-            {
-                // Si le perso atteint pu dépasse sa limite de saut OU si il touche ka map, il ne saute plus.
-                perso.jump(false);
-            }
+                if (perso.getPersoPos().Y <= limiteSaut || maps[i].hititBox.Intersects(perso.getPersoHitBox()))
+                {
+                    // Si le perso atteint pu dépasse sa limite de saut OU si il touche ka map, il ne saute plus.
+                    perso.jump(false);
+                }
 
-            if (!IntersectPixels(mapCollison, mapTextureData, perso.getPersoHitBox(), persoTextureData))
-            {
-                // Si le perso ne touche pas la map, on considère qu'il ne marche pas
-                perso.walking = false;
+                if (!maps[i].hititBox.Intersects(perso.getPersoHitBox()))
+                {
+                    // Si le perso ne touche pas la map, on considère qu'il ne marche pas
+                    perso.walking = false;
+                }
             }
 
 
@@ -261,87 +265,6 @@ namespace Economy
                   0);                                         // Profondeur
         }
 
-
-
-
-
-        static bool IntersectPixels(Rectangle rectangleA, Color[] dataA,
-                                 Rectangle rectangleB, Color[] dataB)
-        {
-            // Find the bounds of the rectangle intersection
-            int top = Math.Max(rectangleA.Top, rectangleB.Top);
-            int bottom = Math.Min(rectangleA.Bottom, rectangleB.Bottom);
-            int left = Math.Max(rectangleA.Left, rectangleB.Left);
-            int right = Math.Min(rectangleA.Right, rectangleB.Right);
-
-            // Check every point within the intersection bounds
-            for (int y = top; y < bottom; y++)
-            {
-                for (int x = left; x < right; x++)
-                {
-                    // Get the color of both pixels at this point
-                    Color colorA = dataA[(x - rectangleA.Left) +
-                                         (y - rectangleA.Top) * rectangleA.Width];
-                    Color colorB = dataB[(x - rectangleB.Left) +
-                                         (y - rectangleB.Top) * rectangleB.Width];
-
-                    // If both pixels are not completely transparent,
-                    if (colorA.A != 0 && colorB.A != 0)
-                    {
-                        // then an intersection has been found
-                        return true;
-                    }
-                }
-            }
-
-            // No intersection found
-            return false;
-        }
-
-        public static Rectangle GetSmallestRectangleFromTexture(Texture2D Texture)
-        {
-            //Create our index of sprite frames
-            Color[,] Colors = TextureTo2DArray(Texture);
-
-            //determine the min/max bounds
-            int x1 = 9999999, y1 = 9999999;
-            int x2 = -999999, y2 = -999999;
-
-            for (int a = 0; a < Texture.Width; a++)
-            {
-                for (int b = 0; b < Texture.Height; b++)
-                {
-                    //If we find a non transparent pixel, update bounds if required
-                    if (Colors[a, b].A != 0)
-                    {
-                        if (x1 > a) x1 = a;
-                        if (x2 < a) x2 = a;
-
-                        if (y1 > b) y1 = b;
-                        if (y2 < b) y2 = b;
-                    }
-                }
-            }
-
-            //We now have our smallest possible rectangle for this texture
-            return new Rectangle(x1, y1, x2 - x1 + 1, y2 - y1 + 1);
-        }
-
-        //convert texture to 2d array
-        private static Color[,] TextureTo2DArray(Texture2D texture)
-        {
-            //Texture.GetData returns a 1D array
-            Color[] colors1D = new Color[texture.Width * texture.Height];
-            texture.GetData(colors1D);
-
-            //convert the 1D array to 2D for easier processing
-            Color[,] colors2D = new Color[texture.Width, texture.Height];
-            for (int x = 0; x < texture.Width; x++)
-                for (int y = 0; y < texture.Height; y++)
-                    colors2D[x, y] = colors1D[x + y * texture.Width];
-
-            return colors2D;
-        }
 
 
     }
