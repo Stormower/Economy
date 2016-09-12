01dac39b0ebb7e709c50b28e2ce24beba19f4db2
Nicolas <Nicolas@NICOLAPTOP>
2016-09-12 21:46:26 +0200

    Changement majeurs au niveau de rafraichissement permettant l'inclusion de plus nombreuses plateformes

--- Economy/Economy/Game1.cs
+++ Economy/Economy/Game1.cs
@@ -27,8 +27,10 @@ namespace Economy
         MouseState oldSourisState;
         Perso perso = new Perso();
         mob[] Mob = new mob[2];
-        Map[] maps = new Map[5];
+        Map[] maps = new Map[16];
         bool b = true;
+        int vitChutte;
+        bool refreshImages = true;
 
    //     int sol = 250;
         float limiteSaut;
@@ -86,11 +88,12 @@ namespace Economy
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+            this.TargetElapsedTime = TimeSpan.FromSeconds(1.0f / 60.0f);
 
-            for (int i = 0; i < maps.Length; i++)
+            foreach (Map map in maps)
             {
 
-                if (maps[i].hitBox.Intersects(perso.getPersoHitBox()) && perso.getPersoPos().Y > limiteSaut)
+                if (map.hitBox.Intersects(perso.getPersoHitBox()) && perso.getPersoPos().Y > limiteSaut)
                 {
                     // Si le personnage touche la map ET que sa position est plus basse que sa limite de saut c'est qu'il a touché le sol ALORS
                     // Il marche
@@ -101,15 +104,15 @@ namespace Economy
                     break;
                 }
 
-                if (!maps[i].hitBox.Intersects(perso.getPersoHitBox()))
+                if (!map.hitBox.Intersects(perso.getPersoHitBox()))
                 {
                     // Si le perso ne touche pas la map, on considère qu'il ne marche pas
                     perso.walking = false;
                 }
 
-                if (perso.getPersoPos().Y <= limiteSaut || maps[i].hitBox.Intersects(perso.getPersoHitBox()))
+                if (perso.getPersoPos().Y <= limiteSaut || map.hitBox.Intersects(perso.getPersoHitBox()) || vitChutte == 2)
                 {
-                    // Si le perso atteint pu dépasse sa limite de saut OU si il touche ka map, il ne saute plus.
+                    // Si le perso atteint pu dépasse sa limite de saut OU si il touche la map, il ne saute plus. (ou si on presse s)
                     perso.jump(false);
                 }
             }
@@ -125,58 +128,76 @@ namespace Economy
                 {
                     // Si le perso est en train de sauter
                     // Il monte
-                    perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y - 4 / maps.Length));
+                    if (refreshImages)
+                        perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y - 6));
                     b = false;
                 }
-               
 
-                
+              if (b)
+               {
+                if (refreshImages)
+                    perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y + 10 * vitChutte));
+                    b = false;
+                }
 
-                
-            
-
-            if (b)
-            {
-                perso.movePerso(new Vector2(perso.getPersoPos().X, perso.getPersoPos().Y + 5));
-                b = false;
-            }
-
-            uInput();
-            base.Update(gameTime);
+                uInput();
+             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-
+            
             spawn(personnage, perso.getPersoPos(), 0, 1f); //On affiche le personnage
 
             spawn(maps[0].texture, new Vector2(150, 400), 0, 1f);                                                   // On affiche chaque carré de map
-            maps[0].hitBox = new Rectangle(150, 400, 64, 1);                                                        // Et on définit leur hitbox
+            maps[0].hitBox = new Rectangle(150, 400, 64, 10);                                                        // Et on définit leur hitbox
             spawn(maps[1].texture, new Vector2(300, 400), 0, 1f);                                                  
-            maps[1].hitBox = new Rectangle(300, 400, 64, 1);      
+            maps[1].hitBox = new Rectangle(300, 400, 64, 10);      
             spawn(maps[2].texture, new Vector2(600, 400), 0, 1f);                                                 
-            maps[2].hitBox = new Rectangle(600, 400, 64, 1);
+            maps[2].hitBox = new Rectangle(600, 400, 64, 10);
             spawn(maps[3].texture, new Vector2(485, 200), 0, 1f);
-            maps[3].hitBox = new Rectangle(485, 200, 64, 1);
-            spawn(maps[4].texture, new Vector2(750, 250), 0, 1f);
-            maps[4].hitBox = new Rectangle(750, 250, 64, 1);
-            spawn(maps[3].texture, new Vector2(300, 200), 0, 1f);
-            maps[3].hitBox = new Rectangle(300, 200, 64, 1);
+            maps[3].hitBox = new Rectangle(485, 200, 64, 10);
+            spawn(maps[4].texture, new Vector2(750, 300), 0, 1f);
+            maps[4].hitBox = new Rectangle(750, 300, 64, 1);
+            spawn(maps[5].texture, new Vector2(300, 200), 0, 1f);
+            maps[5].hitBox = new Rectangle(300, 200, 64, 1);
+            spawn(maps[6].texture, new Vector2(750 - 90, 200), 0, 1f);
+            maps[6].hitBox = new Rectangle(750 - 90, 200, 64, 10);
+            spawn(maps[7].texture, new Vector2(600, 400), 0, 1f);
+            maps[7].hitBox = new Rectangle(600, 400, 64, 10);
+            spawn(maps[8].texture, new Vector2(485, 200), 0, 1f);
+            maps[8].hitBox = new Rectangle(485, 200, 64, 10);
+            spawn(maps[9].texture, new Vector2(750, 300), 0, 1f);
+            maps[9].hitBox = new Rectangle(750, 300, 64, 1);
+            spawn(maps[10].texture, new Vector2(300 + 64 , 400), 0, 1f);
+            maps[10].hitBox = new Rectangle(300 + 64, 400, 64, 1);
+            spawn(maps[11].texture, new Vector2(300, 400), 0, 1f);
+            maps[11].hitBox = new Rectangle(300, 400, 64, 10);
+            spawn(maps[12].texture, new Vector2(600 - 64, 400), 0, 1f);
+            maps[12].hitBox = new Rectangle(600 - 64, 400, 64, 10);
+            spawn(maps[13].texture, new Vector2(485 + 64, 200), 0, 1f);
+            maps[13].hitBox = new Rectangle(485 + 64, 200, 64, 10);
+            spawn(maps[14].texture, new Vector2(750 - 64, 350), 0, 1f);
+            maps[14].hitBox = new Rectangle(750 - 64, 350, 64, 1);
+            spawn(maps[15].texture, new Vector2(364, 200), 0, 1f);
+            maps[15].hitBox = new Rectangle(364, 200, 64, 1);
 
 
             if (perso.getSens() == 1) //Si le perso est dans le sens 1
             {
                 personnage = perso.perso;
                 perso.createPersoHitBox(new Rectangle((int)perso.getPersoPos().X + 24, (int)perso.getPersoPos().Y + 53, 10, 10));
-                perso.moveAttack(new Vector2(perso.getPersoPos().X + 50, perso.getPersoPos().Y)); //et on positionne l'endroit o� appara�tra son attaque si il attaque
+                if (refreshImages)
+                    perso.moveAttack(new Vector2(perso.getPersoPos().X + 50, perso.getPersoPos().Y)); //et on positionne l'endroit o� appara�tra son attaque si il attaque
             }
             else  //Si il est dans le sens 0
             {
                 personnage = perso.perso2;
                 perso.createPersoHitBox(new Rectangle((int)perso.getPersoPos().X + 24, (int)perso.getPersoPos().Y + 53, 10, 10));
-                perso.moveAttack(new Vector2(perso.getPersoPos().X - 50, perso.getPersoPos().Y)); //idem
+                if (refreshImages)
+                    perso.moveAttack(new Vector2(perso.getPersoPos().X - 50, perso.getPersoPos().Y)); //idem
             }
             
             for (int i = 0; i <= 1; i++)
@@ -200,7 +221,7 @@ namespace Economy
             if (perso.getAttOrNot()) //Si le perso attaque
             {
                 spawn(perso.attack, perso.getAttPos(), 0, 1f); //On spawn l'attaque du perso
-                perso.createAttackHitBox(new Rectangle((int)perso.getPersoPos().X, (int)perso.getPersoPos().Y, 64, 64)); // On cr�e sa hitbox
+                perso.createAttackHitBox(new Rectangle((int)perso.getAttPos().X, (int)perso.getAttPos().Y, 64, 64)); // On cr�e sa hitbox
                 for (int i = 0; i <= 1; i++)
                 {
                     if (perso.getAttackHitBox().Intersects(Mob[i].getMobHitBox())) //Si elle touche le mob
@@ -214,7 +235,8 @@ namespace Economy
 
             for (int i = 0; i <= 1; i++)
             {
-                Mob[i].moveMob(new Vector2(Mob[i].getMobPos().X - 1, Mob[i].getMobPos().Y)); //On déplace le mob d'un pixel a chaque refresh
+                if (refreshImages)
+                    Mob[i].moveMob(new Vector2(Mob[i].getMobPos().X - 1, Mob[i].getMobPos().Y)); //On déplace le mob d'un pixel a chaque refresh
             }
             spriteBatch.End();
             base.Draw(gameTime);
@@ -227,6 +249,7 @@ namespace Economy
                     Mob[i].killMob(true);  //Le réssusiter                       //////////////////////
                 }
             }
+            refreshImages = !refreshImages;
         }
 
         private void uInput()
@@ -236,12 +259,14 @@ namespace Economy
 
             if (newState.IsKeyDown(Keys.Q))
             {
-                perso.movePerso(new Vector2(perso.getPersoPos().X - 3, perso.getPersoPos().Y));
+                if (refreshImages)
+                    perso.movePerso(new Vector2(perso.getPersoPos().X - 6, perso.getPersoPos().Y));
                 perso.changeSens(0);
             }
             else if (newState.IsKeyDown(Keys.D))
             {
-                perso.movePerso(new Vector2(perso.getPersoPos().X + 3, perso.getPersoPos().Y));
+                if (refreshImages)
+                  perso.movePerso(new Vector2(perso.getPersoPos().X + 6, perso.getPersoPos().Y));
                 perso.changeSens(1);
             }
 
@@ -260,6 +285,15 @@ namespace Economy
                 Mob[1].moveMob(new Vector2(600, 250));
             }
 
+            if (newState.IsKeyDown(Keys.S))
+            {
+                vitChutte = 2;
+            }
+
+            else
+            {
+                vitChutte = 1;
+            }
             oldState = newState;
 
             if (sourisState.LeftButton == ButtonState.Pressed && oldSourisState.LeftButton != ButtonState.Pressed)
